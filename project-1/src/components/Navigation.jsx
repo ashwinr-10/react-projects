2da99781512bff6c355ab873b9f708b7ff92cb84
ashwinr-10 <jashwinrao31@gmail.com>
2025-05-15 19:45:17 +0000

    #1Main

---
+++ project-1/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+
+    return (
+        <nav>
+        <div className="logo">
+          <img src="/Images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contacts</li>
+        </ul>
+
+        <button>login</button>
+      </nav>
+    );
+}; 
+
+export default Navigation;
